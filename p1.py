864b657fc918dfc554b1ae1544b73a4fd1860999
Hemanthkumar-V <93283260+Hemanthkumar-V@users.noreply.github.com>
2023-10-04 15:13:07 +0530

    program to print heart using python

--- p1.py
+++ p1.py
@@ -1,2 +1,13 @@
 import os
-print("hi hello")
+print("Hello world")
+for i in range (6):
+    for j in range (7):
+        if (i==0 and j%3!=0) or (i==1 and j%3==0) or (i-j==2) or (i+j==8):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+
+print("mentors are well trained and motivative buddies")
+print("this is an jenkins assinment using python")
+print("thankyou nandri vanakam")
